20016cc547963c36b7ebadeae7165931510d18aa
Bernardo-Favoreto <bernardo.pelisson@gmail.com>
2020-07-26 14:51:46 -0300

    new imports

--- src/components/Navbars/CustomNavbar.js
+++ src/components/Navbars/CustomNavbar.js
@@ -2,17 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 // reactstrap components
 import {
-  Button,
-  Collapse,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
   NavbarBrand,
   Navbar,
-  NavItem,
-  NavLink,
-  Nav,
   Container,
   UncontrolledTooltip,
 } from "reactstrap";
